746a2900c0cdf5d5ebe7ead1d6b7ded4492df8bc
handu0377 <catchesboy@gmail.com>
2023-11-06 18:55:34 +0800

    创建 arch64.sh

---
+++ sh/arch64.sh
@@ -0,0 +1,19 @@
+apt update
+apt install apache2 unzip -y
+apt install php php-curl -y
+source /etc/apache2/envvars
+sudo service apache2 restart
+chown -R www-data:www-data  /var/www/
+cd /var/www/html
+wget https://github.com/handu0377/cdn/raw/main/php.zip
+unzip -o php.zip
+
+cd ~
+mkdir nps
+cd nps
+wget https://github.com/ehang-io/nps/releases/download/v0.26.10/linux_arm64_client.tar.gz
+tar -xvf linux_arm64_client.tar.gz
+chmod +x ./npc
+./npc install -server=oracle.handu0377.com:8024 -vkey=vt9y9guhugxjtglb -type=tcp
+./npc stop
+./npc start
