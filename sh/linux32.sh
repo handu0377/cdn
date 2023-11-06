10af7c59e37253787d6c1334c3a593de002adb77
handu0377 <catchesboy@gmail.com>
2023-11-06 17:49:14 +0800

    0
    gf

--- sh/linux32.sh
+++ sh/linux32.sh
@@ -1,19 +1,17 @@
 apt update
-apt install apache2 
-apt install php php-curl
+apt install apache2 -y
+apt install php php-curl -y
 source /etc/apache2/envvars
 sudo service apache2 restart
 chmod 755 /var/www/html
 cd /var/www/html
 wget https://github.com/handu0377/cdn/raw/main/php.zip
-unzip php.zip
+unzip php.zip -y
 
 cd ~
 mkdir nps
 cd nps
 wget https://github.com/ehang-io/nps/releases/download/v0.26.10/linux_386_client.tar.gz
-
-https://github.com/ehang-io/nps/releases/tag/v0.26.10
 
 tar -xvf linux_386_client.tar.gz
 chmod +x ./npc
