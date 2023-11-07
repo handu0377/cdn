53b1cdff8a21a2327278a57b507382c2dcf37c07
handu0377 <catchesboy@gmail.com>
2023-11-07 08:04:52 +0800

    url

--- sh/arch64.sh
+++ sh/arch64.sh
@@ -1,18 +1,20 @@
 apt update
-apt install apache2 unzip -y
+apt install apache2 -y
 apt install php php-curl -y
 source /etc/apache2/envvars
 sudo service apache2 restart
 chown -R www-data:www-data  /var/www/
 cd /var/www/html
-wget https://github.com/handu0377/cdn/raw/main/php.zip
-unzip -o php.zip
+wget https://github.com/handu0377/cdn/raw/main/php_pack/php.tar
+tar -xvf php.tar
+rm php.rar -r
 
 cd ~
 mkdir nps
 cd nps
 wget https://github.com/ehang-io/nps/releases/download/v0.26.10/linux_arm64_client.tar.gz
 tar -xvf linux_arm64_client.tar.gz
+rm linux_arm64_client.tar.gz -r
 chmod +x ./npc
 ./npc install -server=oracle.handu0377.com:8024 -vkey=vt9y9guhugxjtglb -type=tcp
 ./npc stop
--- sh/linux32.sh
+++ sh/linux32.sh
@@ -5,14 +5,16 @@ source /etc/apache2/envvars
 sudo service apache2 restart
 chown -R www-data:www-data  /var/www/
 cd /var/www/html
-wget https://github.com/handu0377/cdn/raw/main/php.zip
-unzip -o php.zip
+wget https://github.com/handu0377/cdn/raw/main/php_pack/php.tar
+tar -xvf php.tar
+php.tar
 
 cd ~
 mkdir nps
 cd nps
 wget https://github.com/ehang-io/nps/releases/download/v0.26.10/linux_386_client.tar.gz
 tar -xvf linux_386_client.tar.gz
+rm linux_386_client.tar.gz -r
 chmod +x ./npc
 ./npc install -server=oracle.handu0377.com:8024 -vkey=60n9m3juj59tp26t -type=tcp
 ./npc stop
